bd361ae1270aa75f32950c1afaa54c112ca4d66a
Sakutaa <mirija.rajoeliarivony-andriamahery@efrei.net>
2022-11-17 11:03:54 +0100

    Début du projet

---
+++ fonction.py
@@ -0,0 +1,32 @@
+def menu_1():
+    print()
+
+    choice1 = 0
+    while choice1 != "3":
+        print("Entrez 1: Jouer dans un cercle")
+        print("Entrez 2: Jouer dans un losange")
+        print("Entrer 3: Jouer dans un triangle")
+        choice1 = str(input("Entrez votre réponse: "))
+        while choice1 < "1" or choice1 > "3":
+            choice1 = str(input("Réponse incorrect. Ressaisissez votre réponse svp : "))
+        if choice1 != "3":
+            return choice1
+    return choice1
+
+
+def grille_jeu_losange():
+    with open("Losange.txt", "r") as f:
+        for l in f:
+            for j in l:
+                if j == '0':
+                    print(" ", end="")
+                else:
+                    print(".", end="")
+            print("\n")
+
+def menu_logo():
+    print("========================================================================")
+    print("||                                                                    ||")
+    print("||                                                                    ||")
+    print("||                                                                    ||")
+    print("========================================================================")
---
+++ main.py
@@ -0,0 +1,11 @@
+from fonction import menu_1, grille_jeu_losange
+
+choice1="0"
+while choice1!="3":
+    choice1 = menu_1()
+    #if choice1 =="1":
+
+
+
+    if choice1 == '2':
+        grille_jeu_losange()
